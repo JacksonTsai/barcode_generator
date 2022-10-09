fb96ab6913505293b067208dda4f36ff7b995f3c
Jackson <yj.gooogle@gmail.com>
2022-10-09 21:55:32 +0800

    feat: read file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import * as JsBarcode from 'jsbarcode';
+
+interface PreviewContent {
+  id: string;
+  filename: string;
+  invoiceNO: string;
+  // barcodeObject: { id: string; code: string };
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'g2330';
+  selectedFiles: File[] = [];
+  previewContent: PreviewContent[] = [];
+  @HostListener('change', ['$event.target.files']) emitFiles(event: FileList) {
+    if (event) {
+      this.selectedFiles = Array.from(event);
+    }
+  }
+
+  previewFiles() {
+    this.selectedFiles.map((file) => {
+      this.fileReader(file);
+    });
+  }
+
+  removeFile(file) {
+    const newFileList = this.selectedFiles.filter((d) => d.name !== file.name);
+    this.selectedFiles = newFileList;
+  }
+
+  private async fileReader(file: File) {
+    const fileReader = new FileReader();
+    fileReader.onerror = (e) => console.log(`${file.name} load fail(${e})`);
+    fileReader.onload = (d) => {
+      this.fileContext(file.name, d);
+    };
+    await fileReader.readAsText(file);
+  }
+
+  private fileContext = (filename, data) => {
+    const code = this.getInvoiceNO(data.target.result);
+    this.previewContent.push({
+      id: `barcode_${code}`,
+      filename,
+      invoiceNO: code,
+    });
+    setTimeout(() => {
+      this.generterBarcode(`#barcode_${code}`, code);
+    }, 0);
+  };
+
+  private getInvoiceNO(data: string) {
+    const temp = data.split(/: |  /);
+    const invoiceIdx = temp.findIndex((d) => d.includes('INVOICE NO'));
+    return temp[invoiceIdx + 1].trim();
+  }
+
+  generterBarcode(id, code: string) {
+    JsBarcode(id, code, {
+      lineColor: '#000',
+      width: 1,
+      height: 40,
+      displayValue: true,
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatStepperModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatDividerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
